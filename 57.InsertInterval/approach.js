bf6a2f792bddab73bff60321aa3c7f5384e4735b
nahidhossain123 <nahidhossain351@gmail.com>
2024-03-18 00:03:37 +0600

    leetcode insert interval problem

---
+++ 57.InsertInterval/approach.js
@@ -0,0 +1,49 @@
+var insert = function (intervals, newInterval) {
+  let merged = [];
+  let i = 0;
+
+  while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+    merged.push(intervals[i]);
+    i++;
+  }
+
+  while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
+    newInterval = [
+      Math.min(newInterval[0], intervals[i][0]),
+      Math.max(newInterval[1], intervals[i][1]),
+    ];
+    i++;
+  }
+  merged.push(newInterval);
+
+  while (i < intervals.length) {
+    merged.push(intervals[i]);
+    i++;
+  }
+
+  return merged;
+};
+
+console.log(
+  "Test Case 1:",
+  insert(
+    [
+      [1, 3],
+      [6, 9],
+    ],
+    [2, 5]
+  )
+);
+console.log(
+  "Test Case 2:",
+  insert(
+    [
+      [1, 2],
+      [3, 5],
+      [6, 7],
+      [8, 10],
+      [12, 16],
+    ],
+    [4, 8]
+  )
+);
