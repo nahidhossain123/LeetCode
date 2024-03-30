beb6f121c260d433089b61c3fbc30db3d247a47a
nahidhossain123 <nahidhossain351@gmail.com>
2024-03-30 23:48:25 +0600

    leetcode problem subarray with different integers sliding wind solution

---
+++ 992.SubarrayswithKDifferentIntegers/approach.js
@@ -0,0 +1,28 @@
+var subarraysWithKDistinct = function (nums, k) {
+  const slidingWindow = (nums, k) => {
+    let i = 0,
+      j = 0,
+      count = 0;
+    const map = new Map();
+
+    while (j < nums.length) {
+      map.set(nums[j], (map.get(nums[j]) || 0) + 1);
+
+      while (map.size > k) {
+        map.set(nums[i], map.get(nums[i]) - 1);
+        if (map.get(nums[i]) === 0) map.delete(nums[i]);
+        i++;
+      }
+
+      count += j - i + 1;
+      j++;
+    }
+
+    return count;
+  };
+
+  return slidingWindow(nums, k) - slidingWindow(nums, k - 1);
+};
+
+console.log("Test Case 1:", subarraysWithKDistinct([1, 2, 1, 2, 3], 2));
+console.log("Test Case 2:", subarraysWithKDistinct([1, 2, 1, 3, 4], 3));
